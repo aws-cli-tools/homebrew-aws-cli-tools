6415236cdf5462dcebc06f4cdd8859d1a828f536
Serverless development in Hebrew <support@sharatless.cloud>
2023-07-14 18:10:13 +0300

    Create whoami.rb

---
+++ whoami.rb
@@ -0,0 +1,11 @@
+class Recon < Formula
+      desc "The `whoami` CLI is a small Rust application that allows users to identify their current AWS identity."
+      homepage "https://github.com/aws-cli-tools"
+      url "__URL__"
+      version "__VERSION__"
+      sha256 "__SHA__"
+
+      def install
+        bin.install "whoami"
+      end
+    end

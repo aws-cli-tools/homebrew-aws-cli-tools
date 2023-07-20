3f69e76656ae2139063d3e0266021b34cb68a3ce
Serverless development in Hebrew <support@sharatless.cloud>
2023-07-20 12:01:16 +0300

    rustwrap update: delete-bucket.rb

--- delete-bucket.rb
+++ delete-bucket.rb
@@ -1,9 +1,9 @@
 class DeleteBucket < Formula
   desc "This is a command-line tool to delete specified AWS S3 buckets"
   homepage "https://github.com/aws-cli-tools/delete-bucket"
-  url "https://github.com/aws-cli-tools/delete-bucket/releases/download/v0.1.4/delete-bucket-v0.1.4-x86_64-macos.tar.xz"
-  version "v0.1.4"
-  sha256 "72f4386f94c323cab632657160dd40807c2e7e524a28d026e1065ab630c0f2ec"
+  url "https://github.com/aws-cli-tools/delete-bucket/releases/download/v0.1.5/delete-bucket-v0.1.5-x86_64-macos.tar.xz"
+  version "v0.1.5"
+  sha256 "df53ca02b8cca2656fe0114cb5d9ffc7adaff713456b22d508ae5453391d1ab2"
 
   def install
     bin.install "delete-bucket"

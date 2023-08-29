61c77980c1b712c28b7f12ba7d3d4518f5518661
Having fun with Serverless <support@sharatless.cloud>
2023-08-29 18:20:14 +0300

    rustwrap update: whoami.rb

--- whoami.rb
+++ whoami.rb
@@ -1,9 +1,9 @@
 class Whoami < Formula
   desc "The `whoami` CLI is a small Rust application that allows users to identify their current AWS identity."
   homepage "https://github.com/aws-cli-tools/whoami"
-  url "https://github.com/aws-cli-tools/whoami/releases/download/v0.1.9/whoami-v0.1.9-x86_64-macos.tar.xz"
-  version "v0.1.9"
-  sha256 "de4607ddd651f3322c97b1b53699bfc287fd10fcde083be7e2b51c61dbda4aa4"
+  url "https://github.com/aws-cli-tools/whoami/releases/download/v0.1.10/whoami-v0.1.10-x86_64-macos.tar.xz"
+  version "v0.1.10"
+  sha256 "fad1f7e562f7e6d8fb45ad2aa663accdcb6d6a97a9dc6a1059f17db93c7ad311"
 
   def install
     bin.install "whoamiaws"
